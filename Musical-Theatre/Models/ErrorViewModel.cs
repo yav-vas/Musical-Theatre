9262f9b6ea843dce6684d8606e50cb6fedfcab20
Iavor Vassilev <yav.vasilev@gmail.com>
2023-02-26 16:02:21 +0200

    Default ASP.NET MVC Web app template

---
+++ Musical-Theatre/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Musical_Theatre.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
