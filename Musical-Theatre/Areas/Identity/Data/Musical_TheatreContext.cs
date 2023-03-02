b3f69e5dd2d9ff8a3d8bd7bb55cdff7cb11c7074
Iavor Vassilev <yav.vasilev@gmail.com>
2023-03-02 22:06:40 +0200

    Default Identity scaffolding
    See ScaffoldingReadMe.txt

---
+++ Musical-Theatre/Areas/Identity/Data/Musical_TheatreContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Musical_Theatre.Areas.Identity.Data;
+
+namespace Musical_Theatre.Data;
+
+public class Musical_TheatreContext : IdentityDbContext<Musical_TheatreUser>
+{
+    public Musical_TheatreContext(DbContextOptions<Musical_TheatreContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
--- Musical-Theatre/Program.cs
+++ Musical-Theatre/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Musical_Theatre.Areas.Identity.Data;
+using Musical_Theatre.Data;
+
 namespace Musical_Theatre
 {
 	public class Program
@@ -5,6 +10,11 @@ namespace Musical_Theatre
 		public static void Main(string[] args)
 		{
 			var builder = WebApplication.CreateBuilder(args);
+			var connectionString = builder.Configuration.GetConnectionString("Musical_TheatreContextConnection") ?? throw new InvalidOperationException("Connection string 'Musical_TheatreContextConnection' not found.");
+
+			builder.Services.AddDbContext<Musical_TheatreContext>(options => options.UseSqlServer(connectionString));
+
+			builder.Services.AddDefaultIdentity<Musical_TheatreUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<Musical_TheatreContext>();
 
 			// Add services to the container.
 			builder.Services.AddControllersWithViews();
@@ -29,6 +39,7 @@ namespace Musical_Theatre
 			app.MapControllerRoute(
 				name: "default",
 				pattern: "{controller=Home}/{action=Index}/{id?}");
+			app.MapRazorPages();
 
 			app.Run();
 		}
